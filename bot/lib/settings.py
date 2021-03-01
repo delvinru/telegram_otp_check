181794c1dfafffebc7de5f31a463cbab09a8210a
Alexey <delvinru@gmail.com>
2021-03-02 00:10:15 +0300

    revert changes

--- bot/lib/settings.py
+++ bot/lib/settings.py
@@ -5,8 +5,9 @@ Also in this file you can setup settings for work with bot.
 """
 import os
 
-TOKEN = os.getenv("OTP_CHECK_TOKEN")
-BOT_NAME = 'kks_checker_bot'
+TOKEN = os.getenv('OTP_CHECK_TOKEN')
+
+BOT_NAME='kks_checker_bot'
 BOT_URL = f'https://t.me/{BOT_NAME}?start='
 
 # REFRESH TIME FOR UPDATING QR CODE
